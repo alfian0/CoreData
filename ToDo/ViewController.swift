76d4be62b432f5f804ce44843ceb22df52299809
alfian0 <alfian0@alfian0s-MacBook-Pro.local>
2017-12-16 09:47:36 +0700

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by alfian0 on 12/16/17.
+//  Copyright © 2017 alfian0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
